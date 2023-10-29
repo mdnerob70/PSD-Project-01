4671c777494bddc9a20ae8995d30f14a0062e8b1
mdnerob70 <nerobislam0462@gmail.com>
2023-10-29 22:45:11 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Bx Slider
+$(document).ready(function(){
+  $(".slider").bxSlider();
+});
+
+// min menu jquery plugins
+$(document).ready(function () {
+  $('.main_menu').meanmenu({
+    meanMenuContainer: '#mobile_menu',
+    meanScreenWidth: 1191, 
+  });
+});
